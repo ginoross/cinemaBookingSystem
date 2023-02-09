0197a8e4564df258280bc5dbfebba7624394f0a0
gr211714 <gr211714@reigate.ac.uk>
2023-02-09 15:50:23 +0000

    updated admin film options

--- src/main/java/cbs/viewFilmsPageController.java
+++ src/main/java/cbs/viewFilmsPageController.java
@@ -2,14 +2,44 @@ package cbs;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class viewFilmsPageController {
+public class viewFilmsPageController implements Initializable {
+
+    ArrayList<File> fileList = new ArrayList<File>();
+    HBox hBox = new HBox();
 
     @FXML
     Button backButton;
+
+    @FXML
+    ScrollPane scrollPane;
+
+    public void initialize(URL location, ResourceBundle resources){
+        try{
+            String path = URLDecoder.decode("src/main/resources/cbs/filmPosters/", StandardCharsets.UTF_8);
+            File folder = new File(path);
+
+            for(File file: folder.listFiles()){
+                fileList.add(file);
+            }
+
+            scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
 
     public void backButtonClicked(ActionEvent event) throws IOException {
